37fdc72ceda961458d27346be1f3bb203d5f7336
David R <davidrobertnunez@gmail.com>
2017-03-24 20:03:27 +0100

    Update MyAlgorithm.py

--- TeachingRobotics/src/color_filter/MyAlgorithm.py
+++ TeachingRobotics/src/color_filter/MyAlgorithm.py
@@ -104,7 +104,7 @@ class MyAlgorithm(threading.Thread):
 
         if input_image is not None:
             self.camera.setColorImage(input_image)
-            self.camera.setThresoldImage(filtroRojo)
+            self.camera.setThresholdImage(filtroRojo)
 
             '''
             If you want show a thresold image (black and white image)
